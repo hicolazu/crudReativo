ac5bd741f441418585013bb3a04573c9948bd266
Henrico <hicolazu22@hotmail.com>
2020-07-03 20:57:33 -0300

    INITIAL COMMIT

---
+++ src/main/java/br/com/henricolazuroz/crudReativo/CrudReativoApplication.java
@@ -0,0 +1,13 @@
+package br.com.henricolazuroz.crudReativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudReativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudReativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/henricolazuroz/crudReativo/CrudReativoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.henricolazuroz.crudReativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudReativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
